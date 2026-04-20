d8d2798a7406c9b47f7f82a4bf82fcc75ff03515
kcrachapudi <kcrachapudi@gmail.com>
2026-04-19 23:44:12 -0500

    main to home

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="DE Portfolio | Kalyan Rachapudi", layout="wide")
 
 # 2. Navigation Definition
 pg = st.navigation([
-    st.Page("main.py", title="Home", icon="🏠", default=True), # Points to your root file
+    st.Page("home.py", title="Home", icon="🏠", default=True), # Points to your root file
     st.Page("pages/1_BioMedTech.py", title="BioMedTech", icon="🧬"),
     st.Page("pages/2_FinTech.py", title="FinTech", icon="💰"),
     st.Page("pages/3_DataAnalytics.py", title="Data Analytics", icon="📊")
