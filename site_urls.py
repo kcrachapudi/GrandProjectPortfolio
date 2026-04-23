13ac62814c0111d072b037a38c99896111b1c7e9
kcrachapudi <kcrachapudi@gmail.com>
2026-04-23 16:25:46 -0500

    Added BioInform2

--- site_urls.py
+++ site_urls.py
@@ -2,7 +2,8 @@
 projects = [
     {"title": "MediStream-AI-Ready-Medical-Imaging-Pipeline", "category": "BioMedTech", "url": "https://medistream-ai-ready-medical-imaging-pipeline.streamlit.app/", "tech": ["Python", "pydicom", "pandas"], "desc": "Automated extraction of lab results into a HIPAA-compliant Postgres warehouse."},
     {"title": "BioInformatics Project 1", "category": "BioMedTech", "url": "https://bioinform1.streamlit.app/", "tech": ["Python", "FastAPI"], "desc": "Bio Informatics Project 1"},
-    
+    {"title": "BioInformatics Project 2", "category": "BioMedTech", "url": "https://bioinform2.streamlit.app/", "tech": ["Python", "FastAPI"], "desc": "Bio Informatics Project 2"},
+
     {"title": "Real-time Ledger Sync", "category": "FinTech", "url": "https://url3.com", "tech": ["Kafka", "Go", "Postgres"], "desc": "Distributed system for synchronizing multi-currency ledgers with 99.99% uptime."},
     {"title": "Fraud Detection Engine", "category": "FinTech", "url": "https://url4.com", "tech": ["Spark", "Python", "Scikit-Learn"], "desc": "Streaming analytics pipeline to identify suspicious transaction patterns."},
 
