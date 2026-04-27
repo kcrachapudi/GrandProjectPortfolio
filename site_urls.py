60bc2183f9a4e144de4b425155bfcd33e8abfc1f
kcrachapudi <kcrachapudi@gmail.com>
2026-04-27 16:03:39 -0500

    Updated Fintech 1st Project

--- site_urls.py
+++ site_urls.py
@@ -4,7 +4,7 @@ projects = [
     {"title": "BioInformatics Project 1", "category": "BioMedTech", "url": "https://bioinform1.streamlit.app/", "tech": ["Python", "FastAPI"], "desc": "Bio Informatics Project 1"},
     {"title": "BioInformatics Project 2", "category": "BioMedTech", "url": "https://bioinform2.streamlit.app/", "tech": ["Python", "FastAPI"], "desc": "Bio Informatics Project 2"},
 
-    {"title": "Real-time Ledger Sync", "category": "FinTech", "url": "https://url3.com", "tech": ["Kafka", "Go", "Postgres"], "desc": "Distributed system for synchronizing multi-currency ledgers with 99.99% uptime."},
+    {"title": "Smart Credit Risk & Loan Eligibility Engine", "category": "FinTech", "url": "https://smartcreditriskloaneligibilityengine.streamlit.app/", "tech": ["Pythona", "Pandas", "AIML"], "desc": "Smart Credit Risk & Loan Eligibility Engine."},
     {"title": "Fraud Detection Engine", "category": "FinTech", "url": "https://url4.com", "tech": ["Spark", "Python", "Scikit-Learn"], "desc": "Streaming analytics pipeline to identify suspicious transaction patterns."},
 
     {"title": "Recruiter Dashboard Analytics", "category": "DataAnalytics", "url": "https://rad-kalyan-rachapudi-recruiter.streamlit.app/", "tech": ["Python", "Pandas", "Plotly"], "desc": "Recruiter Analytics Dashboard for visualizing hiring metrics."},
